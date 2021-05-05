2f2e4dd3ad875ffb7b51e50401ca75ade0a5d028
Floha <floha2011@hotmail.de>
2021-05-05 18:40:36 +0200

    basic chart

--- useflags.py
+++ useflags.py
@@ -3,9 +3,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 p = portage.db[portage.root]["porttree"].dbapi
-portage_versions = p.match("sys-apps/portage")
+a = "sys-apps/portage"
+portage_versions = p.match(a)
+print(portage_versions)
 stats = dict()
 
+#for package in p.cp_all():
 for v in portage_versions:
     flags = p.aux_get(v, ["IUSE"])[0].split(' ')
     for flag in flags:
@@ -14,7 +17,10 @@ for v in portage_versions:
         else:
             stats[flag] = 1
 
-useflags = pd.json_normalize(stats)
-useflags.plot()
+print(stats)
+x = stats.keys()
+y = stats.values();
+plt.bar(x, y)
+plt.savefig("output.jpg")
 
 
